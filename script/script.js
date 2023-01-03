8b2c0336dbffb0488400bd52582d9f6b9c668a9e
jailtonj <jailtonpereira762@gmail.com>
2023-01-03 09:28:32 -0300

    Corrigindo if else

--- script/script.js
+++ script/script.js
@@ -3,13 +3,14 @@ function carregar () {
     var img = window.document.getElementById('imagem')
 
     var data = new Date()
+    // var hora = data.getHours()
     var hora = data.toLocaleTimeString()
     // faz o tempo atualizar automático
     const tempo = setInterval(carregar, 1000)
-    // var hora = data.getHours()
+    
 
     msg.innerHTML =`Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
+    if (hora >= 0 && hora < 12) { 
         // BOM DIA!
         img.src = './assets/manhã.jpg'
         document.body.style.background = '#FBA77A'
@@ -17,7 +18,7 @@ function carregar () {
         // BOA TARDE!
         img.src = './assets/tarde.jpg'
         document.body.style.background = '#B49581'
-    } else {
+    } else if (hora < 0 && hora > 18) {
         // BOA NOITE!
         img.src = './assets/noite.jpg'
         document.body.style.background = '#0C1F2E'
